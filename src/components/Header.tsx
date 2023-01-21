565ffe7a4cf639aac8e7f9961201b901cf97913f
Justin Weisberg <juhstinn@gmail.com>
2023-01-20 17:53:51 -0800

    fixed mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-// import Party from "../../public/rambo-party.svg";
 import MenuIcon from "@mui/icons-material/Menu";
 
 function Header({ ramboHeadActivate }: any) {
   return (
     <div
       className="flex justify-center h-16 fixed
-    p-3  mx-auto max-w-7xl  bg-black w-screen z-10 left-0 right-0 bg-filter backdrop-blur-xl bg-opacity-80 z-20 "
+    p-3  mx-auto max-w-7xl  bg-black w-screen left-0 right-0 bg-filter backdrop-blur-xl bg-opacity-80 z-20 "
     >
       <button className="cursor-pointer absolute left-2 md:left-4  w-10 h-10 ">
         <MenuIcon className="  text-4xl text-white" />
       </button>
 
-      <div className="flex flex-row w-64 md:w-80 ml-5  ">
+      <div className="flex flex-row  justify-center  ">
         <h1 className="text-md md:text-lg mr-1 mt-2 text-white" text-white>
           Rambo
         </h1>
-        <img src="rambo-party.svg" />
+        <img src="rambo-party.svg" className="w-1/3 " />
         <h1 className="text-md md:text-lg  mt-2 text-white">Productions</h1>
       </div>
       <button className="cursor-pointer absolute top-0 right-2 md:right-4  w-10 h-10 ">
         <img
           src="rambo-head.png"
-          className=" w-8 md:w-10"
+          className=" w-8 mt-1 md:mt-0 md:w-10 "
           onClick={() => ramboHeadActivate()}
         />
       </button>
